d25fee32242320613b33bea8ea8e53dcd025b597
Aditya Kumar <adityakumar9074@gmail.com>
2026-04-28 23:58:15 +0530

    2 change

--- Lec-12/app.js
+++ Lec-12/app.js
@@ -7,8 +7,15 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 app.get('/', (req, res)=>{
-    res.render("index.ejs")
+    res.render("index")
+}
+)
+app.get('/read', (req, res)=>{
+    res.render("read")
 })
 
 app.listen('3000');
