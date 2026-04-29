db4fc847b7c812ba42997506284a88c27cdb0308
Aditya Kumar <adityakumar9074@gmail.com>
2026-04-30 02:16:56 +0530

    this is mini backend project  for learning purpose

--- Lec-12/app.js
+++ Lec-12/app.js
@@ -19,6 +19,22 @@ app.get('/read', async (req, res)=>{
    let users = await userModel.find();
     res.render("read", {users});
 })
+
+
+app.get('/edit/:userid', async (req, res)=>{
+   let user = await userModel.findOne({_id: req.params.userid});
+    res.render("edit", {user});
+})
+
+
+app.post('/update/:userid', async (req, res)=>{
+    let {name, email, image} = req.body;
+   let user = await userModel.findOneAndUpdate({_id: req.params.userid}, {name, email, image}, {new:true});
+    res.redirect("/read");
+})
+
+
+
 app.get('/delete/:id', async (req, res)=>{
    let users = await userModel.findOneAndDelete({_id: req.params.id});
     res.redirect("/read");
